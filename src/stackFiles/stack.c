8215f73adbcbe70189b5f58c06644f36f9d8a981
deppy0 <61898784+deppy0@users.noreply.github.com>
2025-06-20 01:38:36 +0800

    finished, debugged and documented

--- src/stackFiles/stack.c
+++ src/stackFiles/stack.c
@@ -1,2 +1,116 @@
-//This file contains implementation for the stack, using stack.h
-//Function implementations here
+#include "stack.h"
+
+// Creates a new stack instance
+void screate(int_sp *stack) { *stack = calloc(1, sizeof(struct _INTSTACKT)); }
+
+// Checks if full
+bool sfull(int_sp stack) { return MAX_STACK == stack->_DO_NOT_MODIFY_COUNT; }
+
+// Checks if empty
+bool sempty(int_sp stack) { return 0 == stack->_DO_NOT_MODIFY_COUNT && NULL == stack->next; }
+
+stackerr_t spush(int_sp *pstack, int value)
+{
+	// Declarations
+	int_sp stack, u;
+	stackerr_t result;
+	// Check if pointer is valid
+	result = pstack != NULL && *pstack != NULL;
+	if (result)
+	{
+		// Para di nakakalito
+		stack = *pstack;
+		// If empty, just change head value
+		if (sempty(stack))
+		{
+			assert(stack->value = malloc(sizeof(int)));
+			*stack->value = value;
+			stack->_DO_NOT_MODIFY_COUNT++;
+		}
+		// If not empty, allocate a new head and attach current head
+		else if (!sfull(stack))
+		{
+			screate(&u);
+			assert(u->value = malloc(sizeof(int)));
+			*u->value = value;
+			u->_DO_NOT_MODIFY_COUNT = stack->_DO_NOT_MODIFY_COUNT + 1;
+			u->next = stack;
+			*pstack = u;
+		}
+		else
+			result = STACK_FULL; // Stack must be full then
+	}
+	else
+		result = STACK_ERROR; // Something's wrong with the parameter
+	return result;
+}
+
+stackerr_t spop(int_sp *pstack, int *out)
+{
+	// Declarations
+	int_sp pTemp, stack;
+	stackerr_t result;
+	// Check if pointer is valid
+	result = pstack != NULL && *pstack != NULL;
+	if (result)
+	{
+		// Para di nakakalito
+		stack = *pstack;
+		// Check if empty
+		result = !sempty(stack);
+		if (result)
+		{
+			// Check if the head is the only node of the stack
+			// If so, simply remove the value of the head
+			if (1 == stack->_DO_NOT_MODIFY_COUNT)
+			{
+				*out = *stack->value;
+				free(stack->value);
+				stack->_DO_NOT_MODIFY_COUNT--;
+				stack->next = NULL;
+			}
+			else
+			{
+				pTemp = stack->next;
+				*out = *stack->value;
+				free(stack->value);
+				free(stack);
+				*pstack = pTemp;
+			}
+		}
+		else
+			result = STACK_EMPTY; // Stack must be empty then
+	}
+	else; // Something's wrong with the parameter
+	return result;
+}
+
+// Returns top value if successful, otherwise returns STACK_ERROR
+int sfirst(int_sp stack) { return stack != NULL && stack->value != NULL ? *stack->value : STACK_ERROR; }
+
+// Returns second value from the top if successful, otherwise returns STACK_ERROR
+int ssecond(int_sp stack) { return stack != NULL && stack->next != NULL ? *stack->next->value : STACK_ERROR; }
+
+stackerr_t sdestroy(int_sp *pstack)
+{
+	int pop;
+	stackerr_t result;
+	result = pstack != NULL && *pstack != NULL;
+	if (result)
+	{
+		// Pop until end is reached
+		while (spop(pstack, &pop) == 1);
+		// Check if all nodes are popped
+		if ((result = sempty(*pstack)))
+			// If stack is empty, then it can be set free accordingly
+			free(*pstack), *pstack = NULL, pstack = NULL, result = 1;
+		else
+			// Otherwise, just return STACK_ERROR since the stack must have 
+			// been interfered with an outside function causing it to not work as intended
+			result = STACK_ERROR;
+	}
+	else; // Stack is already destroyed
+
+	// 0 if stack is already destroyed, STACK_ERROR if stack is hijacked, 1 if successful
+	return result;
+}
--- src/stackFiles/stack.h
+++ src/stackFiles/stack.h
@@ -1,2 +1,29 @@
-// The header file containing details about the stack implementation
-// Function declarations here
+#include "stdlib.h"
+#include "assert.h"
+#include "stdbool.h"
+
+// Max stack is 2^15 = 32768
+#define MAX_STACK 1<<15
+
+// Error codes
+#define STACK_ERROR -0x57ACC077 // "STAC(K) CORR(upted)". Stack is invalid or not working as intended
+#define STACK_FULL  -0X57ACFFFF // "STAC(K) FFFF". Stack has reached its upper limit
+#define STACK_EMPTY -0x57AC0000 // "STAC(K) 0x0000". Stack has 0x0000 elements
+
+typedef struct _INTSTACKT
+{
+	int _DO_NOT_MODIFY_COUNT;
+	int *value;
+	struct _INTSTACKT *next;
+} *int_sp;
+
+typedef int stackerr_t;
+
+void screate(int_sp *stack);
+bool sfull(int_sp stack);
+bool sempty(int_sp stack);
+stackerr_t spush(int_sp *pstack, int value);
+stackerr_t spop(int_sp *pstack, int *out);
+int sfirst(int_sp stack);
+int ssecond(int_sp stack);
+stackerr_t sdestroy(int_sp *pstack);
