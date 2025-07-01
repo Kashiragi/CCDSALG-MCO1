34d92fc74ecd3a34d4b88b964f737b74e87f6706
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-07-01 19:39:31 +0800

    fix(main): fixed skeleton to allow handling of points to be in array, instead of stacks. > note: graham scan will return points in an array hence the changes

---
+++ src/main_fast.c
@@ -0,0 +1,43 @@
+#include "graham_fast.h"
+
+int main()
+{
+    // declarations
+    char inFilename[256], outFilename[256];
+    int sampleSize, hullSize, i=0;
+    Point inPoint, temp_point;
+    Point inPointStk[32768], *arrHullPts;
+    FILE *inFile, *outFile;
+    
+    printf("GRAHAM SCAN - Fast Version\n");
+    // file handling, to avoid other programs open
+    printf("Give me CORRECT filename for input (ex. input1.txt): ");
+    scanf("%s", inFilename);
+    printf("Give me CORRECT filename for output(ex. output1.txt): ");
+    scanf("%s", outFilename);
+    
+    // Input points
+    inFile = fopen(inFilename, "r");
+    fscanf(inFile, "%d", &sampleSize);
+    while(i<sampleSize && fscanf(inFile, "%lf %lf", &inPoint.x, &inPoint.y)==2){
+        inPointStk[i] = inPoint;
+        i++;
+    }
+    fclose(inFile);
+
+    //code for graham scan, incorporating sort and stack funcs
+    printf("Fast graham scan in progress...\n");
+
+
+    // if you can, after graham, free inPointStk, then alloc space for arrHullPts kasi 32768 elements
+    // otherwise, just declare 32768 for them both sa taas na agad tysm
+   
+    // Output hull points
+    outFile = fopen(outFilename, "w");
+    fprintf(outFile, "*Number of Points: %d\n", hullSize);
+    while(i<hullSize && fprintf(outFile, "%.6lf %.6lf", arrHullPts[i].x, arrHullPts[i].y)==2){
+        i++;
+    }
+    fclose(outFile);
+    return 0;
+}
