87eac64c7bec68e0eec77d6548b52b81b33b9ca0
Savie888 <queenie_mae_salao@dlsu.edu.ph>
2025-07-02 14:09:11 +0800

    Update sort.h

--- src/sortFiles/sort.h
+++ src/sortFiles/sort.h
@@ -1,27 +1,19 @@
+#include "globals.h"
 #ifndef SORT_H
 #define SORT_H
-#include "../globals.h"
 
-typedef struct {
-    double x;
-    double y;
-} Point;
-
-extern Point anchor;
 
 double computePolarAngle(Point p1, Point p2);
 double computeDistance(Point p1, Point p2);
-
 int comparePoints(Point a, Point b);
 
 void selectionSort(Point points[], int n);
 void mergeSort(Point points[], int n);
-
 void mergeSortHelper(Point points[], Point temp[], int left, int right);
 void merge(Point points[], Point temp[], int left, int mid, int right);
 
 void findAndSetAnchor(Point points[], int n);
-
 void sortPointsByPolarAngle(Point points[], int n, int useSelectionSort);
 
-#endif 
+#endif // SORT_H
+
